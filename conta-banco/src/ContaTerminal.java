03c98b335a8d62ac9646eaa9aa52f02cf92074bd
Samindran <matheus.acioli.v@gmail.com>
2024-09-09 10:20:59 -0300

    Criação da conta e exibição de mensagens

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,25 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+        System.out.println("Por favor, digite o número da Conta:");
+        int numeroConta = scanner.nextInt();
+        scanner.nextLine();
+
+        System.out.println("Por favor, digite o número da Agência:");
+        String numeroAgencia = scanner.nextLine();
+
+        System.out.println("Por favor, digite o Nome Completo:");
+        String nomeCliente = scanner.nextLine();
+
+        System.out.println("Por favor, digite o Saldo:");
+        double saldoConta = scanner.nextDouble();
+
+        System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é "
+                + numeroAgencia + ", conta " + numeroConta + " e seu saldo " + saldoConta
+                + " já está disponível para saque.");
     }
 }
